b7060c6a6b1fe6dbcd50837355aa21a40ed82aff
Mohamed Asik Rahman <40391581+Ashikrahman123@users.noreply.github.com>
2023-05-30 23:05:15 +0530

    Update ts.js

--- ts.js
+++ ts.js
@@ -1,7 +1,19 @@
 const restartButton =
 document.querySelector(".reset");
-restartButton.addEventListener("click",()=> {
-    const textAnimation = document.querySelector(".text-stroke");
-    setAnimationName(textAnimation,"none");
-  }
-)
+restartButton.addEventListener("click",
+ ()=> {
+const textAnimation =
+document.querySelector(".text-stroke");
+setAnimationName(textAnimation,"none");
+requestAnimationFrame(()=>
+setTimeout(()=> setAnimationName(textAnimation,""),0)
+);
+    },
+    false
+);
+
+const setAnimationName= (element, animationName)=> {
+    if (element) {
+        element.style.animationName = animationName;
+    }
+};
